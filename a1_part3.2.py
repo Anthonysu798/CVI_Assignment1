382f895a60f58daeeefc8a5b81a6ca3e3a1c14d4
Anthony Su <113278092+Anthonysu798@users.noreply.github.com>
2025-02-08 10:19:26 -0500

    Assignment part3

---
+++ a1_part3.2.py
@@ -0,0 +1,15 @@
+#We, Anthony Su and Yingyong L. (mention assigned group number and your names), declare that the attached assignment is our own work in accordance with the Seneca Academic Policy.  We have not copied any part of this assignment, manually or electronically, from any other source including web sites, unless specified as references. We have not distributed our work to other students.
+import cv2 as cv
+
+# Read the image
+img = cv.imread('lambo.jpg')
+
+# Draw a filled green rectangle
+# Parameters: image, start_point, end_point, color (BGR format), thickness
+cv.rectangle(img, (100, 100), (500, 400), (0, 255, 0), thickness=-1)  # -1 means filled rectangle
+
+# Display the image
+cv.imshow("Sports Car", img)
+
+cv.waitKey(0)
+
---
+++ a1_part3.3.py
@@ -0,0 +1,20 @@
+#We, Anthony Su and Yingyong L. (mention assigned group number and your names), declare that the attached assignment is our own work in accordance with the Seneca Academic Policy.  We have not copied any part of this assignment, manually or electronically, from any other source including web sites, unless specified as references. We have not distributed our work to other students.
+import cv2 as cv
+
+# Read the image
+img = cv.imread('lambo.jpg')
+
+# Draw a green rectangle
+# Parameters: image, start_point, end_point, color (BGR format), thickness
+cv.rectangle(img, (100, 100), (500, 400), (0, 255, 0), thickness=4)
+
+# Add text on the image
+# Parameters: image, text, position, font, scale, color, thickness
+cv.putText(img, 'Lamborghini', (120, 150), cv.FONT_HERSHEY_SIMPLEX, 
+          1.0, (255, 255, 255), thickness=2)
+
+# Display the image
+cv.imshow("Sports Car", img)
+
+cv.waitKey(0)
+
